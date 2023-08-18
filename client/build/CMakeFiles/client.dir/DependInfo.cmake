155813a79898b23e5443444f184369f88a1605d7
ylx <561639418@qq.com>
2023-08-18 09:49:23 +0800

    ooo

---
+++ client/build/CMakeFiles/client.dir/DependInfo.cmake
@@ -0,0 +1,31 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/wlx/chatroom-wlx/client/src/Err.cpp" "CMakeFiles/client.dir/src/Err.cpp.o" "gcc" "CMakeFiles/client.dir/src/Err.cpp.o.d"
+  "/home/wlx/chatroom-wlx/client/src/Group_menu.cpp" "CMakeFiles/client.dir/src/Group_menu.cpp.o" "gcc" "CMakeFiles/client.dir/src/Group_menu.cpp.o.d"
+  "/home/wlx/chatroom-wlx/client/src/Massage.cpp" "CMakeFiles/client.dir/src/Massage.cpp.o" "gcc" "CMakeFiles/client.dir/src/Massage.cpp.o.d"
+  "/home/wlx/chatroom-wlx/client/src/Printf.cpp" "CMakeFiles/client.dir/src/Printf.cpp.o" "gcc" "CMakeFiles/client.dir/src/Printf.cpp.o.d"
+  "/home/wlx/chatroom-wlx/client/src/clirun.cpp" "CMakeFiles/client.dir/src/clirun.cpp.o" "gcc" "CMakeFiles/client.dir/src/clirun.cpp.o.d"
+  "/home/wlx/chatroom-wlx/client/src/clitmain.cpp" "CMakeFiles/client.dir/src/clitmain.cpp.o" "gcc" "CMakeFiles/client.dir/src/clitmain.cpp.o.d"
+  "/home/wlx/chatroom-wlx/client/src/file.cpp" "CMakeFiles/client.dir/src/file.cpp.o" "gcc" "CMakeFiles/client.dir/src/file.cpp.o.d"
+  "/home/wlx/chatroom-wlx/client/src/friend_menu.cpp" "CMakeFiles/client.dir/src/friend_menu.cpp.o" "gcc" "CMakeFiles/client.dir/src/friend_menu.cpp.o.d"
+  "/home/wlx/chatroom-wlx/client/src/mainmnue.cpp" "CMakeFiles/client.dir/src/mainmnue.cpp.o" "gcc" "CMakeFiles/client.dir/src/mainmnue.cpp.o.d"
+  "/home/wlx/chatroom-wlx/client/src/manmage_menu.cpp" "CMakeFiles/client.dir/src/manmage_menu.cpp.o" "gcc" "CMakeFiles/client.dir/src/manmage_menu.cpp.o.d"
+  "/home/wlx/chatroom-wlx/client/src/privateChat.cpp" "CMakeFiles/client.dir/src/privateChat.cpp.o" "gcc" "CMakeFiles/client.dir/src/privateChat.cpp.o.d"
+  "/home/wlx/chatroom-wlx/client/src/recv.cpp" "CMakeFiles/client.dir/src/recv.cpp.o" "gcc" "CMakeFiles/client.dir/src/recv.cpp.o.d"
+  "/home/wlx/chatroom-wlx/client/src/sign.cpp" "CMakeFiles/client.dir/src/sign.cpp.o" "gcc" "CMakeFiles/client.dir/src/sign.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ server/build/CMakeFiles/server.dir/DependInfo.cmake
@@ -0,0 +1,31 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/wlx/chatroom-wlx/server/src/Err.cpp" "CMakeFiles/server.dir/src/Err.cpp.o" "gcc" "CMakeFiles/server.dir/src/Err.cpp.o.d"
+  "/home/wlx/chatroom-wlx/server/src/Group.cpp" "CMakeFiles/server.dir/src/Group.cpp.o" "gcc" "CMakeFiles/server.dir/src/Group.cpp.o.d"
+  "/home/wlx/chatroom-wlx/server/src/Group_menu.cpp" "CMakeFiles/server.dir/src/Group_menu.cpp.o" "gcc" "CMakeFiles/server.dir/src/Group_menu.cpp.o.d"
+  "/home/wlx/chatroom-wlx/server/src/Massage.cpp" "CMakeFiles/server.dir/src/Massage.cpp.o" "gcc" "CMakeFiles/server.dir/src/Massage.cpp.o.d"
+  "/home/wlx/chatroom-wlx/server/src/User.cpp" "CMakeFiles/server.dir/src/User.cpp.o" "gcc" "CMakeFiles/server.dir/src/User.cpp.o.d"
+  "/home/wlx/chatroom-wlx/server/src/file.cpp" "CMakeFiles/server.dir/src/file.cpp.o" "gcc" "CMakeFiles/server.dir/src/file.cpp.o.d"
+  "/home/wlx/chatroom-wlx/server/src/friends_menu.cpp" "CMakeFiles/server.dir/src/friends_menu.cpp.o" "gcc" "CMakeFiles/server.dir/src/friends_menu.cpp.o.d"
+  "/home/wlx/chatroom-wlx/server/src/mainmenu.cpp" "CMakeFiles/server.dir/src/mainmenu.cpp.o" "gcc" "CMakeFiles/server.dir/src/mainmenu.cpp.o.d"
+  "/home/wlx/chatroom-wlx/server/src/manage_menu.cpp" "CMakeFiles/server.dir/src/manage_menu.cpp.o" "gcc" "CMakeFiles/server.dir/src/manage_menu.cpp.o.d"
+  "/home/wlx/chatroom-wlx/server/src/privatechat.cpp" "CMakeFiles/server.dir/src/privatechat.cpp.o" "gcc" "CMakeFiles/server.dir/src/privatechat.cpp.o.d"
+  "/home/wlx/chatroom-wlx/server/src/serun.cpp" "CMakeFiles/server.dir/src/serun.cpp.o" "gcc" "CMakeFiles/server.dir/src/serun.cpp.o.d"
+  "/home/wlx/chatroom-wlx/server/src/servermain.cpp" "CMakeFiles/server.dir/src/servermain.cpp.o" "gcc" "CMakeFiles/server.dir/src/servermain.cpp.o.d"
+  "/home/wlx/chatroom-wlx/server/src/sign.cpp" "CMakeFiles/server.dir/src/sign.cpp.o" "gcc" "CMakeFiles/server.dir/src/sign.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
